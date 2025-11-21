8720e819dbb9a51baa7fa298b0fe3c4d300299df
Roman Manzhelii <145562170+Roman-Manzhelii@users.noreply.github.com>
2025-11-21 21:46:43 +0000

    delete Weather add simple api/students/ping route

---
+++ UniversityAPI/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityAPI.Models;
+
+namespace UniversityAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentsController : ControllerBase
+{
+    private readonly UniversityContext _context;
+
+    public StudentsController(UniversityContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/students/ping
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok(new { message = "Students API is working" });
+    }
+}
